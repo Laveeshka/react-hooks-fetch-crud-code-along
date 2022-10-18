0420a462257b4ebec588feac5eb870785076459c
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-18 23:34:20 +1100

    complete code along

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,40 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  //since we are trying to update the items state, we should call the useEffect hook here in the ShoppingList component to initiate the fetch request
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then(res => res.json())
+      .then((items) => setItems(items))
+  }, []);
+
+  //callback function to pass as a prop to ItemForm component to add a new item
+  function handleAddItem(newItem) {
+    console.log("In Shopping List: ", newItem);
+    setItems([...items, newItem]);
+  }
+
+  //callback function to pass as a prop to Item to update an item
+  function handleUpdateitem(updatedItem){
+    //console.log("In Shopping Cart: ", updatedItem);
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id){
+        return updatedItem;
+      } 
+      else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  //callback function to pass as a prop to Item to delete an item
+  function handleDeleteItem(deletedItem){
+    console.log("In Shopping Cart: ", deletedItem);
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +53,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateitem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
